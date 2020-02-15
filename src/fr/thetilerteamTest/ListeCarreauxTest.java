d6bf6ba75a2aa7a133919193e9df362d713d8275
sebastien.cuvellier@hotmail.fr <56834108+Saibe1111@users.noreply.github.com>
2020-02-15 19:06:51 +0100

    TestADD

---
+++ src/fr/thetilerteamTest/ListeCarreauxTest.java
@@ -0,0 +1,14 @@
+package fr.thetilerteamTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ListeCarreauxTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
