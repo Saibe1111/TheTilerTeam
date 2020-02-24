f9888f9d98fa2ae76ca61a368f8194f523243338
Sebastien <sebastien.cuvellier@hotmail.com>
2020-02-24 11:36:28 +0100

    Mur avant modif taille

--- src/fr/thetilerteam/test/MurTest.java
+++ src/fr/thetilerteam/test/MurTest.java
@@ -1,15 +1,19 @@
 package fr.thetilerteam.test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import fr.thetilerteam.logique.Carreau;
 import fr.thetilerteam.logique.Mur;
 
 class MurTest {
 
 	private static Mur m;
+	private static Carreau c;
 
 	@BeforeEach
 	void setUp() {
@@ -17,53 +21,63 @@ class MurTest {
 	}
 
 	@Test
-	void testAjoutLigne() {
-		m.ajoutLigne();
-	}
-
-	@Test
 	void testGetMur() {
-
-	}
-
-	@Test
-	void testSetMur() {
-		fail("Not yet implemented");
+		m.setMur(0, 0, 't');
+		assertEquals('t', m.getMur(0, 0));
 	}
 
 	@Test
 	void testAppartientAuMur() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testPlacerCarreauSurMur() {
-		fail("Not yet implemented");
+		m.ajoutLigne();
+		assertTrue(m.appartientAuMur(1, 1));
+		assertFalse(m.appartientAuMur(1, 2));
 	}
 
 	@Test
 	void testNombreNiveauComplet() {
-		fail("Not yet implemented");
+		m.setMur(0, 0, 't');
+		m.setMur(1, 0, 't');
+		m.setMur(2, 0, 't');
+		m.setMur(3, 0, 't');
+		m.setMur(4, 0, 't');
+		assertEquals(1, m.nombreNiveauComplet());
 	}
 
 	@Test
 	void testCaseVide() {
-		fail("Not yet implemented");
+		c = new Carreau(1, 1, 't');
+		m.setMur(0, 0, 't');
+		assertFalse(m.caseVide(0, 0, c), "La case ne devrait pas etre vide");
+		assertTrue(m.caseVide(1, 0, c), "La case devrait etre vide");
 	}
 
 	@Test
 	void testCarreauAdjacent() {
-		fail("Not yet implemented");
+		c = new Carreau(1, 1, 't');
+		m.setMur(0, 0, 's');
+		assertTrue(m.carreauAdjacent(1, 0, c));
+		assertFalse(m.carreauAdjacent(4, 0, c));
 	}
 
 	@Test
 	void testCarreauReposeSurBase() {
-		fail("Not yet implemented");
+		c = new Carreau(3, 1, 't');
+		m.setMur(0, 0, 's');
+		m.setMur(2, 0, 's');
+		assertFalse(m.carreauReposeSurBase(0, 1, c));
+		m.setMur(1, 0, 's');
+		assertTrue(m.carreauReposeSurBase(0, 1, c));
 	}
 
 	@Test
 	void testCarreauClone() {
-		fail("Not yet implemented");
+		c = new Carreau(3, 1, 't');
+		m.setMur(0, 0, 's');
+		m.setMur(2, 0, 's');
+		m.setMur(1, 0, 's');
+		assertFalse(m.carreauClone(0, 1, c));
+		c = new Carreau(2, 1, 't');
+		assertTrue(m.carreauClone(0, 1, c));
 	}
 
 }
